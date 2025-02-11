06fee5b8a90abf3d4d30834c964c59887ccacc0c
Shanalee2 <edrebudan@gmail.com>
2025-02-11 21:23:44 +0800

    Add files via upload

---
+++ greeting-card/script.js
@@ -0,0 +1,7 @@
+function openCard() {
+    document.querySelector('.card-inner').style.transform = 'rotateY(180deg)';
+}
+
+function closeCard() {
+    document.querySelector('.card-inner').style.transform = 'rotateY(0deg)';
+}
