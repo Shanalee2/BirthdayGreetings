2f6092d688618d7391c8f7d09c146b342b10f3ae
Shanalee2 <edrebudan@gmail.com>
2025-02-11 21:23:24 +0800

    Delete greeting-card directory

--- greeting-card/script.js
+++
@@ -1,7 +0,0 @@
-function openCard() {
-    document.querySelector('.card-inner').style.transform = 'rotateY(180deg)';
-}
-
-function closeCard() {
-    document.querySelector('.card-inner').style.transform = 'rotateY(0deg)';
-}
